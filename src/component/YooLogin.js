fb2da7da9450f5609d1693adde9a4db64042f46d
kdxcxs <cx@kdxcxs.com>
2020-11-30 20:12:25 +0800

    feat: init YooLogin with ui

--- src/component/YooLogin.js
+++ src/component/YooLogin.js
@@ -4,9 +4,34 @@ import YooLoginUI from '../ui/YooLoginUI';
 export default class YooLogin extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.setUsername = this.setUsername.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.login = this.login.bind(this);
   }
 
+  setUsername(value) {
+    this.setState({username: value});
+  }
+
+  setPassword(value) {
+    this.setState({password: value});
+  }
+
+  login(onFail) {}
+
   render() {
-    return <YooLoginUI />;
+    return (
+      <YooLoginUI
+        username={this.state.username}
+        password={this.state.password}
+        setUsername={this.setUsername}
+        setPassword={this.setPassword}
+        loginCallback={this.login}
+      />
+    );
   }
 }
