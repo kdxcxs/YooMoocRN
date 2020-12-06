c139df8fdfc5c9f1053abc488f13d1b373715329
kdxcxs <cx@kdxcxs.com>
2020-12-06 13:34:01 +0800

    fix(YooBackground): make background image also move in the first 10 seconds

--- src/ui/YooBackground.js
+++ src/ui/YooBackground.js
@@ -10,6 +10,17 @@ export default function YooBackground() {
       zIndex: -1,
     },
   });
+  // The animation won't play for the first 10 seconds
+  // if we just use setInterval. So we start the animation
+  // at the component mounted just for the first 10 seconds.
+  Animated.timing(imgPosition, {
+    toValue: {
+      x: -Math.floor(Math.random() * 5000),
+      y: -Math.floor(Math.random() * 3000),
+    },
+    duration: 10000,
+    useNativeDriver: true,
+  }).start();
   setInterval(() => {
     Animated.timing(imgPosition, {
       toValue: {
